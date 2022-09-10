8c6cfff98dde55f082ec6763d4e98436c1ae8d9f
mayoyemiwa <mayoyemiwa@gmail.com>
2022-09-10 20:30:13 +0100

    My profile frontend first upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/userRoutes');
 const cookieParser = require('cookie-parser')
+const path = require('path');
 
 
 const app = express();
